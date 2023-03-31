8cbeebfaeec37d5eaa56d36575ebb22a1557cb0a
Piotr <92379337+PWcislo26@users.noreply.github.com>
2023-03-31 11:54:00 +0200

    Initial commit

---
+++ helper.py
@@ -0,0 +1,9 @@
+import textwrap
+
+def wrap_labels(ax, width, break_long_words=False):
+    labels = []
+    for label in ax.get_xticklabels():
+        text = label.get_text()
+        labels.append(textwrap.fill(text, width=width,
+                      break_long_words=break_long_words))
+    ax.set_xticklabels(labels, rotation=0)
